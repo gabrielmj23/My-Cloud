4f5e4d010f9fa14b5a04f10f35b02e566908f16c
gabrielmj23 <gabrielmj2312@gmail.com>
2023-02-18 18:03:23 -0400

    Initial setup with Vite + React + TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Vite + React</h1>
+      <div className="card">
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+}
+
+export default App;
