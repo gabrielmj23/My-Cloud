49fda95ec6ac68d79dcf26e44fdbb02f5ee84d40
gabrielmj23 <gabrielmj2312@gmail.com>
2023-02-19 10:43:44 -0400

    Enable cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 import { downloadsRouter } from "./routes/downloads";
 import { filesRouter } from "./routes/files";
 import { uploadsRouter } from "./routes/uploads";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use('/uploads', uploadsRouter);
 app.use('/downloads', downloadsRouter);
